1e0c3bed2dc54a506f7c64dbb736bae2a92cd61f
Nelson Estevam <nelson.estevam@paygo.com.br>
2020-08-19 13:51:33 -0300

    Deployment test-ppro

---
+++ run.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+echo ""
+echo ""
+echo "##############################"
+echo "# Creating Argocd deployment #"
+echo "##############################"
+k3s kubectl create ns argocd
+k3s kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+
+sleep 10
+echo ""
+echo ""
+echo "#####################################"
+echo "# Creating infrastructure test-ppro #"
+echo "#####################################"
+k3s kubectl apply -f boostrap/
+
+sleep 90
+echo ""
+echo ""
+echo "###################"
+echo "# Endpoint Argocd #"
+echo "###################"
+k3s kubectl -n argocd get ep argocd-server
+
+echo ""
+echo ""
+echo "###########################"
+echo "# Password admin - Argocd #"
+echo "###########################"
+k3s kubectl get pods -n argocd -l app.kubernetes.io/name=argocd-server -o name | cut -d'/' -f 2
+
+echo ""
+echo ""
+echo "########################################"
+echo "# Waiting running deployment test-ppro # "
+echo "########################################"
+sleep 60
+
+echo ""
+echo "##########################"
+echo "# Endpoint test-ppro DEV #"
+echo "##########################"
+k3s kubectl -n dev get ep
+
+echo ""
+echo "###########################"
+echo "# Endpoint test-ppro PROD #"
+echo "###########################"
+k3s kubectl -n prod get ep
