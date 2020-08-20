98e1fe75dd4519e22093721b65f5abf119d9c2ea
Nelson Estevam <nelson.estevam@paygo.com.br>
2020-08-20 11:17:55 -0300

    Fix run.sh remove k3s

--- run.sh
+++ run.sh
@@ -4,11 +4,11 @@ echo ""
 echo "##########################################"
 echo "# Creating infrastrucuture for test-ppro #"
 echo "##########################################"
-k3s kubectl apply -f boostrap/01-mysql-deployment.yaml
-k3s kubectl create ns argocd
-k3s kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+kubectl apply -f boostrap/01-mysql-deployment.yaml
+kubectl create ns argocd
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 sleep 20
-k3s kubectl apply -f boostrap/
+kubectl apply -f boostrap/
 
 sleep 60
 echo ""
@@ -16,14 +16,14 @@ echo ""
 echo "###################"
 echo "# Endpoint Argocd #"
 echo "###################"
-k3s kubectl -n argocd get ep argocd-server
+kubectl -n argocd get ep argocd-server
 
 echo ""
 echo ""
 echo "###########################"
 echo "# Password admin - Argocd #"
 echo "###########################"
-k3s kubectl get pods -n argocd -l app.kubernetes.io/name=argocd-server -o name | cut -d'/' -f 2
+kubectl get pods -n argocd -l app.kubernetes.io/name=argocd-server -o name | cut -d'/' -f 2
 
 echo ""
 echo ""
@@ -36,10 +36,10 @@ echo ""
 echo "##########################"
 echo "# Endpoint test-ppro DEV #"
 echo "##########################"
-k3s kubectl -n dev get ep
+kubectl -n dev get ep
 
 echo ""
 echo "###########################"
 echo "# Endpoint test-ppro PROD #"
 echo "###########################"
-k3s kubectl -n prod get ep
+kubectl -n prod get ep
